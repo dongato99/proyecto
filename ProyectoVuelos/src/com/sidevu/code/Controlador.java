07d9b76fe9e0f5fbc83e67f241b285c0fbcd979e
DELL <DELL@ART-DELL>
2018-06-25 19:05:27 -0500

    Primera parte de rama principal

---
+++ ProyectoVuelos/src/com/sidevu/code/Controlador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sidevu.code;
+
+/**
+ *
+ * @author DELL
+ */
+public class Controlador {
+    public static void main(String args[]) {
+        
+    }
+}
