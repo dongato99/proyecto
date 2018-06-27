1268ea01303410f871149ec83c95a197aae061fc
Usuario <Usuario@Usuario-PC>
2018-06-27 17:12:24 -0500

    Modificacion de la clase "Avion"

--- ProyectoVuelos/src/com/sidevu/code/modelo/Avion.java
+++ ProyectoVuelos/src/com/sidevu/code/modelo/Avion.java
@@ -5,8 +5,15 @@
  */
 package com.sidevu.code.modelo;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * 
@@ -105,4 +112,39 @@ public class Avion {
                             asientosDisponibles.add(i);
 		return asientosDisponibles;
 	}
+        
+        public Avion leerJSON () {
+		Avion avion=null;
+		JSONParser parser = new JSONParser();
+		 
+        try {
+        	FileReader file = new FileReader("./sidevu/aviones/"+nombre+".txt");
+            Object obj = parser.parse(file);
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray strings = (JSONArray) jsonObject.get("lista");    
+            avion = new Avion(strings.get(0).toString().replaceAll("nombre:", ""), 
+            		Integer.parseInt(strings.get(1).toString().replaceAll("cupo:", "")));
+       		file.close();		
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return avion;
+	}
+        
+        public void escribirAJSON() {
+		JSONObject obj = new JSONObject();
+		JSONArray strings = new JSONArray();
+		strings.add("nombre:"+ nombre);
+		strings.add("cupo:"+ cupo);
+		obj.put("lista", strings);
+		try (FileWriter file = new FileWriter("./sidevu/aviones/"+nombre+".txt")) {
+			file.write(obj.toJSONString());
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Object: " + obj);
+			file.close();
+		} catch (IOException e) {	
+			e.printStackTrace();
+		}
+	}
+        
 }
