186a6241f6abdac9b5eea0e840e33b01e9da42a6
USUARIO <USUARIO@DESKTOP-QPQHE2E>
2018-06-27 20:06:47 -0500

    Forzar funcion para tipo entero

--- ProyectoVuelos/src/com/sidevu/code/Utilidades.java
+++ ProyectoVuelos/src/com/sidevu/code/Utilidades.java
@@ -10,5 +10,23 @@ package com.sidevu.code;
  * @author DELL
  */
 public class Utilidades {
-    
+
+    public static boolean esEntero(String s) {
+        try {
+            int num = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static int leerEntero(String mensaje) {
+        try {
+            int num = Integer.parseInt(javax.swing.JOptionPane.showInputDialog(mensaje));
+            return num;
+        } catch (NumberFormatException e) {
+            return leerEntero(mensaje);
+        }
+
+    }
 }
