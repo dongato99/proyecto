cec4e55dae2129bd9856593df123e111418c58e8
Usuario <Usuario@Usuario-PC>
2018-06-29 22:15:17 -0500

    Modificacion de la clase "Controlador"

--- ProyectoVuelos/src/com/sidevu/code/Controlador.java
+++ ProyectoVuelos/src/com/sidevu/code/Controlador.java
@@ -5,6 +5,11 @@
  */
 package com.sidevu.code;
 
+import com.sidevu.code.modelo.Avion;
+import com.sidevu.code.modelo.Vuelo;
+import java.util.ArrayList;
+import java.util.List;
+
 /**a
  *
  * @author DELL
@@ -12,5 +17,30 @@ package com.sidevu.code;
 public class Controlador {
     public static void main(String args[]) {
         
+    }
+    
+    public static Avion obtenerAvion(String text) 
+    {
+		for (Avion v : aviones) {
+			if (text.equalsIgnoreCase(v.obtenerNombre()))
+				return v;
+		}
+		return null;
+    }
+    
+    public static void crearAvion(String nombre, int cupo) 
+    {
+		Avion avion = new Avion(nombre, cupo);
+		eliminarAvion(nombre);
+		aviones.add(avion);
+		avion.escribirAJSON();
+    }
+    
+    public static List<Avion> aviones = new ArrayList<Avion>();// Una lista global con todos los aviones registrados
+    
+    public static List<Vuelo> vuelos = new ArrayList<Vuelo>();// Una lista global con todos los vuelos registrados
+
+    private static void eliminarAvion(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
